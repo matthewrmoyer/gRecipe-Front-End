68cfcd76d9401f81c0b307bfe9ca7a36cc058669
Matthew Moyer <matthewrmoyer@gmail.com>
2017-03-13 15:03:11 -0600

    setup handlebars

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
 
 
+	var allRecipes = [
+		{
+			"id": 1,
+			"name": "Peanut Butter and Jelly",
+			"rating": 3
+		},
+		{
+			"id": 2,
+			"name": "Cereal",
+			"rating": 5
+		}
+	]
 	$("body").on('click', function(){
 		console.log('body clicked')
 	})
+
+
+
+
+	var source   = $("#all-recipes-template").html();
+	var template = Handlebars.compile(source);
+
+
+	for( var i = 0; i<allRecipes.length; i++) {
+		var html = template(allRecipes[i])
+		$('.content-placeholder').append(html)
+	}
+
+
+
+
 
 
 
