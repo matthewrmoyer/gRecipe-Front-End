d1ad669c5ca886ec08395506ac082c239e393410
Matthew Moyer <matthewrmoyer@gmail.com>
2017-03-13 15:42:34 -0600

    homepage setup

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,21 +1,34 @@
 $(document).ready(function() {
 
-
+	//dummy data
 	var allRecipes = [
 		{
 			"id": 1,
 			"name": "Peanut Butter and Jelly",
-			"rating": 3
+			"author": "Steve",
+			"rating": 2
 		},
 		{
 			"id": 2,
 			"name": "Cereal",
+			"author": "Joe",
 			"rating": 5
 		}
 	]
+
+	//sanity test
 	$("body").on('click', function(){
 		console.log('body clicked')
 	})
+
+
+
+	var allRecipesSorted = allRecipes.sort((a,b) =>{
+		var a1 = a.rating
+		var b1 = b.rating
+		return a1<b1? 1: -1
+	});
+	
 
 
 
@@ -23,14 +36,10 @@ $(document).ready(function() {
 	var source   = $("#all-recipes-template").html();
 	var template = Handlebars.compile(source);
 
-
-	for( var i = 0; i<allRecipes.length; i++) {
-		var html = template(allRecipes[i])
-		$('.content-placeholder').append(html)
-	}
-
-
-
+	allRecipesSorted.forEach((element) => {
+		var html = template(element)
+		$('.recipes-placeholder').append(html)
+	})
 
 
 
