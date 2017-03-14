3cb1c94cd2d23d21f66f72e99e7500f203b0b7dc
Matthew Moyer <matthewrmoyer@gmail.com>
2017-03-14 16:36:27 -0600

    deployed to firebase with index create and single all working with ajax to jordans database #hireus

--- public/scripts/create-a-recipe.js
+++ public/scripts/create-a-recipe.js
@@ -15,7 +15,8 @@ $(document).ready(function() {
 		$.post('https://grecipeback.herokuapp.com/recipeRoute', {
 				title: $(".create-a-recipe-name-input").val(),
 				body: $(".create-a-recipe-body-input").val(),
-				image: $(".create-a-recipe-image-input").val()
+				image: $(".create-a-recipe-image-input").val(),
+				email: $(".create-a-recipe-email-input").val()
 			})
 			.done(console.log('post success'))
 			.fail(console.log('post failed'))
