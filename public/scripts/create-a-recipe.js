f3a7902b458c2d9761ca32a7630ba03d6b2b102f
Matthew Moyer <matthewrmoyer@gmail.com>
2017-03-14 13:12:27 -0600

    edit form store data set up post route

--- public/scripts/create-a-recipe.js
+++ public/scripts/create-a-recipe.js
@@ -2,11 +2,36 @@ $(document).ready(function() {
 
 
 	//sanity test
-	$("body").on('click', function(){
-		console.log('body asdadsfafdas')
+	// $("body").on('click', function(){
+	// 	console.log('body asdadsfafdas')
+	// })
+
+
+	$('.create-a-recipe-submit-button').on("click", (event) => {
+		event.preventDefault()
+
+		console.log('create a click')
+
+// var blah = {
+// 				name: $(".create-a-recipe-name-input").val(),
+// 				email: $(".create-a-recipe-username-input").val(),
+// 				blogpostTitle: $(".create-a-recipe-title-input").val(),
+// 				blogpostBody: $(".create-a-recipe-body-input").val(),
+// 				image: $(".create-a-recipe-image-input").val()
+// 			}
+// 			console.log(blah)
+
+
+
+		$.post('/WHATEVER THE RECIPE ROUTE IS', {
+				name: $(".create-a-recipe-name-input").val(),
+				email: $(".create-a-recipe-email-input").val(),
+				blogpostTitle: $(".create-a-recipe-title-input").val(),
+				blogpostBody: $(".create-a-recipe-body-input").val()
+			})
+			.done(console.log('post success'))
+			.fail(console.log('post failed'))
+		//location.reload();
 	})
-
-
-
 
 });
