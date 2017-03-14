21fc881507bda6be717bf511dafa40fb53392243
Matthew Moyer <matthewrmoyer@gmail.com>
2017-03-14 13:34:40 -0600

    accessing jordans API on index.html AWWWWWWWW YEEEAAAAAAA BOOOOOIIIIIIIIII WOOOOO WOOOO WOOOOOO WOOOOOO

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,56 +1,42 @@
 $(document).ready(function() {
 
 	//dummy data
-	var allRecipes = [
-		{
-			"id": 1,
-			"name": "Peanut Butter and Jelly",
-			"author": "Steve",
-			"rating": 2
-		},
-		{
-			"id": 2,
-			"name": "Cereal",
-			"author": "Joe",
-			"rating": 5
-		}
-	]
+	var allRecipes = []
 
 
 
-	/* 
-		$.get('/index', (data) => {
-		data.forEach((element) => {
-			//push to all reciples array to use with handlbars
+	$.get('https://grecipeback.herokuapp.com/recipeRoute', (data) => {
+			data.forEach((element) => {
+				// console.log(element)
+				//push to all reciples array to use with handlbars
+				allRecipes.push(element)
+			})
+			console.log(allRecipes)
+
+
+			var allRecipesSorted = allRecipes.sort((a, b) => {
+				var a1 = a.rating
+				var b1 = b.rating
+				return a1 < b1 ? 1 : -1
+			});
+
+
+
+			var source = $("#all-recipes-template").html();
+			var template = Handlebars.compile(source);
+
+			allRecipesSorted.forEach((element) => {
+				var html = template(element)
+				$('.recipes-placeholder').append(html)
+			})
+
+
+
 		})
-	})*/
 	//sanity test
-	$("body").on('click', function(){
+	$("body").on('click', function() {
 		console.log('body clicked')
 	})
-
-
-
-	var allRecipesSorted = allRecipes.sort((a,b) =>{
-		var a1 = a.rating
-		var b1 = b.rating
-		return a1<b1? 1: -1
-	});
-	
-
-
-
-
-	var source   = $("#all-recipes-template").html();
-	var template = Handlebars.compile(source);
-
-	allRecipesSorted.forEach((element) => {
-		var html = template(element)
-		$('.recipes-placeholder').append(html)
-	})
-
-
-
 
 
 
